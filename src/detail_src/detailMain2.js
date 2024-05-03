1fe2b862234139f1cb61925c44b80a3c48616e1b
Boyoung Lee <genuss040822@gamil.com>
2024-05-03 17:19:21 +0900

    파이어베이스 확인

--- src/detail_src/detailMain2.js
+++ src/detail_src/detailMain2.js
@@ -1,7 +1,7 @@
 import { searchMovies } from "../all_src/searchMovies.js";
 import { toTheTop } from "../all_src/toTheTop.js";
 import { detailInfo } from "./detailInfo.js";
-import { addReview } from "./addReview.js";
+import { addReview } from "./addReview2.js";
 import { getReview } from "./getReview.js";
 import { deleteReview } from "./deleteReview.js";
 import { fixReview } from "./fixReview.js";
