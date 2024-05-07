0ca512682c7ffeb9363d3de07f0b4de6865949b8
Minsu <alstn549@naver.com>
2024-05-07 10:43:24 +0900

    dd

--- src/detail_src/review.js
+++ src/detail_src/review.js
@@ -52,7 +52,6 @@ export const review = async() => {
     reviews.forEach((review) => {
       const id = review["ID"];
       const text = review["text"];
-      console.log[id]
       let temp_html = `<div>작성자: ${id} 리뷰내용: ${text}
       <button class="delete-btn" data-id="${id}">삭제</button></div>`;
       reviewList.innerHTML += temp_html;
