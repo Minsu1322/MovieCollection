026c73f6383540b9734f59b80739955130ddd68f
Minsu <alstn549@naver.com>
2024-05-09 11:04:09 +0900

    finish

--- src/detail_src/getReview.js
+++ src/detail_src/getReview.js
@@ -11,7 +11,7 @@ export const getReview = async (movieId) => {
     const score = review["score"];
     let temp_html = `
       <div>
-        <p id="unserInfo"><span id="userName">${id}</span>&ensp;<span class="date">${date}</span>&ensp;${score}</span></p> 
+        <p id="userInfo"><span id="userName">${id}</span>&ensp;<span class="date">${date}</span>&ensp;${score}</span></p> 
         <p id="text">${text}</p>
         <button class="delete-btn" data-id="${id}">삭제</button>
      </div>`;
